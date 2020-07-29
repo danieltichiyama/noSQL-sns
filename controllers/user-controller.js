5b93f860557279ab95516b0541f30b538653d4a3
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2020-07-28 15:40:59 -1000

    all routes for User-controller working

---
+++ controllers/user-controller.js
@@ -0,0 +1,67 @@
+const { User } = require("../models");
+
+const userController = {
+  getAllUsers(req, res) {
+    User.find({})
+      .select("-__v")
+      .sort({ _id: -1 })
+      .then((results) => res.json(results))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+  getUserById(req, res) {
+    User.findOne({ _id: req.params.id })
+      .select("-__v")
+      .then((results) => {
+        if (!results) {
+          res.status(400).json({ message: "No user found with this id." });
+          return;
+        }
+        res.json(results);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+  createUser(req, res) {
+    User.create(req.body)
+      .then((results) => res.json(results))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+  updateUser(req, res) {
+    User.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
+      .then((results) => {
+        if (!results) {
+          res.status(400).json({ message: "No user found with this id." });
+          return;
+        }
+        res.json(results);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+  deleteUser(req, res) {
+    User.findOneAndDelete({ _id: req.params.id })
+      .then((results) => {
+        if (!results) {
+          res.status(400).json({ message: "No user found with this id." });
+          return;
+        }
+        res.json(results);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+};
+
+module.exports = userController;
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(require("./routes"));
+
+mongoose.connect("mongodb://localhost/noSQL-sns", {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+mongoose.set("debug", true);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
